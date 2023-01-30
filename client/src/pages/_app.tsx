ac82d055544999f2628a5a44b8bad1446e839fdb
cpuccino <thegreatcappuccino@gmail.com>
2023-01-30 22:56:31 +1100

    feat(client): Replace styled-components with scss

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,51 +1,15 @@
 import { AppProps } from "next/app";
 import { StrictMode } from "react";
-import { createGlobalStyle } from "styled-components";
 
-export const Reset = createGlobalStyle`
-  #root, #__next {
-    isolation: isolate;
-  }
-
-  *, *::before, *::after {
-    margin: 0;
-    box-sizing: border-box;
-  }
-  
-  html, body {
-    height: 100%;
-  }
-
-  body {
-    line-height: 1.5;
-    -webkit-font-smoothing: antialiased;
-  }
-
-  img, picture, video, canvas, svg {
-    display: block;
-    max-width: 100%;
-  }
-
-  input, button, textarea, select {
-    font: inherit;
-  }
-
-  p, h1, h2, h3, h4, h5, h6 {
-    overflow-wrap: break-word; 
-    hyphens: auto;
-  }
-`;
+import "../styles/main.scss";
 
 const App = (props: AppProps) => {
 	const { Component, pageProps } = props;
 
 	return (
-		<>
-			<Reset />
-			<StrictMode>
-				<Component {...pageProps} />
-			</StrictMode>
-		</>
+		<StrictMode>
+			<Component {...pageProps} />
+		</StrictMode>
 	);
 };
 
