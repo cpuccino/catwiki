d5ce475e9570e38e9333f221fc9c79fc0da940d3
cpuccino <thegreatcappuccino@gmail.com>
2023-01-31 07:02:24 +1100

    feat(client): Added home banner section

---
+++ client/src/components/home-banner.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+
+import BreedSearch from "./breed-search";
+
+const HomeBanner: FC = () => {
+	return (
+		<div className="c-home-banner">
+			<img
+				className="c-home-banner__background"
+				src="https://img5.goodfon.com/wallpaper/nbig/0/16/chiornyi-fon-kot-vzgliad-koshka-kisa.jpg"
+			/>
+			<div className="c-home-banner__content">
+				<h1 className="c-home-banner__heading is-logo">CatWiki</h1>
+				<h4>Get to know more about your cat breed</h4>
+				<BreedSearch />
+			</div>
+		</div>
+	);
+};
+
+export default HomeBanner;
