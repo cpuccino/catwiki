8b6dc7cd11ba43d6b55938ec4bd83103d5b60912
cpuccino <thegreatcappuccino@gmail.com>
2023-01-31 05:57:27 +1100

    feat(client): Added footer

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Footer: FC = () => {
+	return (
+		<div className="c-footer">
+			<h3 className="is-logo">CatWiki</h3>
+			<p>© 2023 - built by cpuccino</p>
+		</div>
+	);
+};
+
+export default Footer;
