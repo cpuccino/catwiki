49ac7fdf365460d1f013777e9f2e66f681612ce9
cpuccino <thegreatcappuccino@gmail.com>
2023-01-30 16:26:14 +1100

    feat(server): Added middlewares

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,19 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 const main = () => {
-  const app = express();
-  const port = 5000;
-  
-  app.listen(port, () => {
+	const app = express();
+	const port = 5000;
+
+	app.use(cors());
+	app.use(json());
+	app.use(urlencoded({ extended: false }));
+	app.use(morgan("dev"));
+
+	app.listen(port, () => {
 		console.log(`Listening to port ${port}.`);
 	});
-}
+};
 
 main();
