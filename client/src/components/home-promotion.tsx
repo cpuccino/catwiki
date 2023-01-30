cecdf6129126469b8fdf939bcd7e536f94fd1368
cpuccino <thegreatcappuccino@gmail.com>
2023-01-31 07:40:57 +1100

    feat(client): Added home promotion section

---
+++ client/src/components/home-promotion.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { FC } from "react";
+
+const HomePromotion: FC = () => {
+	return (
+		<div className="c-home-promotion">
+			<div className="c-home-promotion__left">
+				<h2 className="c-home-promotion__title is-size-1">Why should you have a cat?</h2>
+				<p className="c-home-promotion__excerpt">
+					Having a cat around you can actually trigger the release of calming chemicals in your body
+					which lower your stress and anxiety levels
+				</p>
+				<Link className="c-home-promotion__read-more" href="/">
+					Read more ⟶
+				</Link>
+			</div>
+			<div className="c-home-promotion__right">
+				<img src="https://images.unsplash.com/photo-1490650034439-fd184c3c86a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80" />
+			</div>
+		</div>
+	);
+};
+
+export default HomePromotion;
