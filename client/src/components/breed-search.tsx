134ecfb88185c17ef2a23623f7f90afb7292b9ae
cpuccino <thegreatcappuccino@gmail.com>
2023-01-31 06:54:29 +1100

    feat(client): Added filtering in breed search

--- client/src/components/breed-search.tsx
+++ client/src/components/breed-search.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, FC, useState } from "react";
 
 import { FaTimes } from "react-icons/fa";
+import { CatBreed } from "src/api/catwiki";
 import { catBreedMock } from "src/mock/cat-breed";
 
 const BreedSearch: FC = () => {
@@ -14,6 +15,20 @@ const BreedSearch: FC = () => {
 		setValue(event.currentTarget.value);
 	};
 
+	const renderPopup = (breeds: CatBreed[]) => {
+		const matches = breeds.filter(breed => breed.name.includes(value));
+
+		if (!matches.length) {
+			return <div className="c-breed-search__popup-empty is-size-4">No results found</div>;
+		}
+
+		return matches.map(breed => (
+			<div key={breed.name} className="c-breed-search__popup-item is-size-4">
+				{breed.name}
+			</div>
+		));
+	};
+
 	return (
 		<div className="c-breed-search">
 			<input
@@ -25,11 +40,7 @@ const BreedSearch: FC = () => {
 			<div className="c-breed-search__empty" onClick={handleOnEmptyClick}>
 				<FaTimes />
 			</div>
-			<div className="c-breed-search__popup">
-				{catBreedMock.map(breed => (
-					<div className="c-breed-search__popup-item is-size-4">{breed.name}</div>
-				))}
-			</div>
+			{value && <div className="c-breed-search__popup">{renderPopup(catBreedMock)}</div>}
 		</div>
 	);
 };
